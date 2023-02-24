0ff30b1aa4af2633768229e416cc57b34680cffe
Mr-PickleVR <122049514+Mr-PickleVR@users.noreply.github.com>
2023-02-24 07:17:09 -0500

    stuff updated

--- pythonGames/Pygame tut/Data/.py Files/main.py
+++ pythonGames/Pygame tut/Data/.py Files/main.py
@@ -1,16 +1,29 @@
+
+#all the imports
 import pygame
 import random
 import time
 import pickle as pkl
+
+#pygame, pygame font and pygame mixer initilizations
 pygame.init()
 pygame.font.init()
+pygame.mixer.init()
 
+#Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
+#SFX initilization
+popSFX = pygame.mixer.Sound("Data/Music/POP.mp3")
+BGMusic = pygame.mixer.Sound("Data/Music/BGmusic.mp3")
+
+
+#screen init
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption('Apple Clicker')
 
+#enemy class for drawing the apple
 class enemy(pygame.sprite.Sprite):
     def __init__(self, x,y, scale, imagePath):
         pygame.sprite.Sprite.__init__(self)
@@ -20,30 +33,36 @@ class enemy(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(img, (int(sizeY),int(sizeX)))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
+    #enemy.set_X(num) to set just the x value
     def set_X(self,newX):
         self.rect.x = newX
+    #enemy.set_Y(num) to set just the y value
     def set_Y(self,newY):
         self.rect.y = newY
+    #enemy.setPos(num,num2) to set both
     def setPos(self,newX,newY):
         self.rect.x = newX
         self.rect.y = newY
 
+#enemy init
 x= 200
 y=200
-
 enemy1 = enemy(300,300,2,"Data/img/apple.png")
 defualt = pygame.font.SysFont('Comic Sans MS', 20)
 end = pygame.font.SysFont("Comic Sans MS", 50)
 endScreenTxtSurface = end.render("Game Over! Resetting Score!(Click To Dismiss)",False,(255,255,255))
 endScreentxtRect = endScreenTxtSurface.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+#several variables for thegame to run properly
 gameover = False
 run = True
 score = 0
 sessionHi = 0
+#pickle import for alltime hi score
 alltimeHi = pkl.load(open("Data/hiScore.txt", 'rb'))
-
+pygame.mixer.music.play(BGMusic, loops= -1)
+#main loop
 while run:
-
+    #screen and text init
     screen.fill(0)
     screen.blit(enemy1.image, enemy1.rect)
     scoreTxt = defualt.render(f"Current Score {score}",False,(255,255,255))
@@ -54,7 +73,7 @@ while run:
     screen.blit(alltimeHiScoreTxt, (10, 90))
 
 
-
+    #event handeler
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
@@ -66,6 +85,7 @@ while run:
             if enemy1.rect.collidepoint(pos) and pressed1:
                 enemy1.setPos(random.randint(100, SCREEN_WIDTH - 100),random.randint(100,SCREEN_HEIGHT - 100))
                 score +=1
+                popSFX.play()
             elif pressed1 and not enemy1.rect.collidepoint(pos):
                 gameover=True
             elif gameover:
